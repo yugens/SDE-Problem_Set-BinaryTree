358303e0e54ad2e66568e999435e87a1a84d02ca
Yashi Srivastava <yashi.18ben1017@abes.ac.in>
2021-04-09 18:40:55 +0530

    Create Inorder Traversal.cpp

---
+++ Inorder Traversal.cpp
@@ -0,0 +1,28 @@
+//Inorder Traversal with recursion
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> ans;
+    void fun(TreeNode* root){
+        if(root == NULL) return;
+        fun(root -> left);
+        ans.push_back(root -> val);
+        fun(root -> right);
+        return;
+    }
+    vector<int> inorderTraversal(TreeNode* root) {
+        fun(root);
+        return ans;
+            
+    }
+};
