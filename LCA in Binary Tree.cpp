7a658f41c01c38ab090f9e82d91ec0a21dc754dd
Yashi Srivastava <yashi.18ben1017@abes.ac.in>
2021-04-15 00:16:40 +0530

    Create LCA in Binary Tree.cpp

---
+++ LCA in Binary Tree.cpp
@@ -0,0 +1,30 @@
+//LCA in Binary Tree
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+       if(root==NULL)
+           return root;
+        if(root==p or root==q)
+            return root;
+        TreeNode* left=lowestCommonAncestor(root->left,p,q);
+        TreeNode* right=lowestCommonAncestor(root->right,p,q);
+        if(left!=NULL and right!=NULL)
+            return root;
+        if(left==NULL and right==NULL)
+            return NULL;
+        if(left!=NULL)
+            return left;
+        else
+            return right;
+    }
+};
